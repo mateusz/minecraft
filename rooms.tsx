86e3cd38e0256ed9397d2c698d5031d1a27b48da
Mateusz Uzdowski <mateusz>
2022-08-21 21:29:24 +1200

    Figure out tiled

---
+++ rooms.tsx
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="rooms" tilewidth="9" tileheight="9" tilecount="23" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="9" height="9" source="rooms_images/sticker_room.png"/>
+ </tile>
+ <tile id="1">
+  <image width="9" height="9" source="rooms_images/factory.png"/>
+ </tile>
+ <tile id="2">
+  <image width="9" height="9" source="rooms_images/castle_dungeon.png"/>
+ </tile>
+ <tile id="3">
+  <image width="9" height="9" source="rooms_images/dining.png"/>
+ </tile>
+ <tile id="4">
+  <image width="9" height="9" source="rooms_images/school.png"/>
+ </tile>
+ <tile id="5">
+  <image width="9" height="9" source="rooms_images/war_room.png"/>
+ </tile>
+ <tile id="6">
+  <image width="9" height="9" source="rooms_images/miners_cave.png"/>
+ </tile>
+ <tile id="7">
+  <image width="9" height="9" source="rooms_images/pokemon_room.png"/>
+ </tile>
+ <tile id="8">
+  <image width="9" height="9" source="rooms_images/dice_room.png"/>
+ </tile>
+ <tile id="9">
+  <image width="9" height="9" source="rooms_images/spider_room.png"/>
+ </tile>
+ <tile id="10">
+  <image width="9" height="9" source="rooms_images/library.png"/>
+ </tile>
+ <tile id="11">
+  <image width="9" height="9" source="rooms_images/lazerforce.png"/>
+ </tile>
+ <tile id="12">
+  <image width="9" height="9" source="rooms_images/castle.png"/>
+ </tile>
+ <tile id="13">
+  <image width="9" height="9" source="rooms_images/swimming_pool.png"/>
+ </tile>
+ <tile id="14">
+  <image width="9" height="9" source="rooms_images/bathroom.png"/>
+ </tile>
+ <tile id="15">
+  <image width="9" height="9" source="rooms_images/train_station.png"/>
+ </tile>
+ <tile id="16">
+  <image width="9" height="9" source="rooms_images/bank.png"/>
+ </tile>
+ <tile id="17">
+  <image width="9" height="9" source="rooms_images/clothes_shop.png"/>
+ </tile>
+ <tile id="18">
+  <image width="9" height="9" source="rooms_images/deadend.png"/>
+ </tile>
+ <tile id="19">
+  <image width="9" height="9" source="rooms_images/corner.png"/>
+ </tile>
+ <tile id="20">
+  <image width="9" height="9" source="rooms_images/straight.png"/>
+ </tile>
+ <tile id="21">
+  <image width="9" height="9" source="rooms_images/tee.png"/>
+ </tile>
+ <tile id="22">
+  <image width="9" height="9" source="rooms_images/intersection.png"/>
+ </tile>
+</tileset>
