ce4efb93b42ec6eef20d738dda1191aa513ed039
Mateusz Uzdowski <mateusz>
2022-08-22 20:23:53 +1200

    Add entities to the map

---
+++ entities.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="entities" tilewidth="1" tileheight="1" tilecount="5" columns="0">
+ <tile id="4">
+  <image width="1" height="1" source="entities/zombie.png"/>
+ </tile>
+ <tile id="5">
+  <image width="1" height="1" source="entities/skeleton.png"/>
+ </tile>
+ <tile id="6">
+  <image width="1" height="1" source="entities/creeper.png"/>
+ </tile>
+ <tile id="7">
+  <image width="1" height="1" source="entities/enderman.png"/>
+ </tile>
+ <tile id="8">
+  <image width="1" height="1" source="entities/spider.png"/>
+ </tile>
+</tileset>
